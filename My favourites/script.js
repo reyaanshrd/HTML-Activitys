4b556f1b27eac717da5e2d7cc3441ddc6aa1202a
reyaanshrd <milkhumama@gmail.com>
2025-06-22 10:58:42 +0530

    commit10

---
+++ My favourites/script.js
@@ -0,0 +1,6 @@
+function imagenext() {
+    document.getElementById ("img1").src="Screenshot 2025-06-21 at 11.37.16 AM.png"
+}
+function foodnext() {
+    document.getElementById ("img3").src="CookbookEggrollTongs.jpg"
+}
